bff17c28015df5635959d22596a867867d74612d
minamitoshiki373-star <minami.toshiki.373@gmail.com>
2026-06-06 23:42:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -52,12 +52,19 @@ if study_data and len(study_data) > 0:
         st.markdown("### 📊 現在のスキル熟練度")
         fig = px.bar(
             df, 
-            x="習得スキル（プロジェクト名）",
+            x="習得スキル（プロジェクト名）", 
             y="稼いだ経験値（分）",
             color="習得スキル（プロジェクト名）",
             text="稼いだ経験値（分）",
             color_discrete_sequence=px.colors.qualitative.Safe
         )
+        
+        fig.update_traces(
+            texttemplate='%{text:.1f} EXP', 
+            textposition='outside',
+            textfont=dict(color='#1e293b')
+        )
+        
         fig.update_layout(
             paper_bgcolor='rgba(0,0,0,0)',
             plot_bgcolor='rgba(0,0,0,0)',
@@ -68,16 +75,11 @@ if study_data and len(study_data) > 0:
                 titlefont=dict(color='#1e293b')
             ),
             xaxis=dict(
-                tickfont=dict(color='#1e293b'), #
+                tickfont=dict(color='#1e293b'),
                 titlefont=dict(color='#1e293b')
             )
         )
-
-        fig.update_traces(
-            texttemplate='%{text:.1f} EXP', 
-            textposition='outside',
-            textfont=dict(color='#1e293b') #
-        )
+        
         st.plotly_chart(fig, use_container_width=True)
 
     with col2:
